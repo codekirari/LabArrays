013cc4dd335a83a06c497c2fd0743428160c5b66
Samary Cardozo <samarycardozo@gmail.com>
2025-10-30 12:54:55 -0500

    add labarrays.js and pdf

---
+++ LabArrays.js
@@ -0,0 +1,5 @@
+// Ejercicio 1: Calculadora de Propinas
+
+// - Una app para calcular propinas en restaurantes.
+// Crea una función calcularPropina(total, porcentaje) que reciba el total de la cuenta y el porcentaje de 
+// propina, y retorne el monto final a pagar (total + propina).
