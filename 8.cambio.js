1aae10743b5aa82ab2b799ed1f525192c0924cf4
Samary Cardozo <samarycardozo@gmail.com>
2025-10-31 09:40:09 -0500

    Primera parte. Punto 8.

--- 8.cambio.js
+++ 8.cambio.js
@@ -2,3 +2,8 @@
 // -App de viajes.
 // Crea una función convertirMoneda(monto, monedaDestino) que convierta de pesos a USD o EUR,
 // pedir al usuario monto en COP y la moneda con la que desea hacer la converción.
+
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let monto = Number(prompt("Ingrese la cantidad en COP: "));
